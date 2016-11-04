bd82da03b672c2247c724275556dba5a72abef17
hzliyuhang <18098085960@163.com>
2016-11-04 17:15:52 +0800

    start logback-slf4j logger with a designated conf file path

--- src/main/java/tool/log/logback/Main.java
+++ src/main/java/tool/log/logback/Main.java
@@ -1,14 +1,35 @@
 package tool.log.logback;
 
 
+import java.net.URL;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.impl.StaticLoggerBinder;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.util.ContextInitializer;
+import ch.qos.logback.core.joran.spi.JoranException;
 
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
+		loggerSystemInit();
+	}
+	
+	
+	public static void loggerSystemInit() throws Exception {
+		/*
+		 * 指定路径启动日志系统
+		 */
+		URL logpath = Main.class.getResource("logback.xml");
+	//	LoggerContext loggerContext = new LoggerContext();
+		LoggerContext loggerContext = (LoggerContext)StaticLoggerBinder.getSingleton().getLoggerFactory();
+		loggerContext.reset();
+		
+		new ContextInitializer(loggerContext).configureByResource(logpath);
 		Logger log = LoggerFactory.getLogger("c");
 		log.info("test1111111111111");
 	}
