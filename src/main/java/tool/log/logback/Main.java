57c0b3a5305e50ce23e55389d3a52b61360a112b
hzliyuhang <18098085960@163.com>
2016-11-04 16:59:54 +0800

    first init

---
+++ src/main/java/tool/log/logback/Main.java
@@ -0,0 +1,16 @@
+package tool.log.logback;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logger log = LoggerFactory.getLogger("c");
+		log.info("test1111111111111");
+	}
+
+}
