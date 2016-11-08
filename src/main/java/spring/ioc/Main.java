3b967f3206963d7d996bb6614d9310198646bbcd
3153242134@qq.com <3153242134@qq.com>
2016-11-08 23:00:29 +0800

    ioc test 1

---
+++ src/main/java/spring/ioc/Main.java
@@ -0,0 +1,15 @@
+package spring.ioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext a = new ClassPathXmlApplicationContext("context.xml");
+		//Bean b =  (Bean) a.getBean("facBean");
+		Object bean = a.getBean("complicatedBean");
+	}
+
+}
---
+++ src/main/java/spring/ioc/RunOnlyOnce.java
@@ -0,0 +1,10 @@
+package spring.ioc;
+
+public class RunOnlyOnce {
+
+
+	
+	public RunOnlyOnce() {
+		System.out.println("spring invoke me! : " + RunOnlyOnce.class);
+	}
+}
