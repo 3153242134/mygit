c4c0632482811b03354c6c1291fc0281d3d4013b
hzliyuhang <18098085960@163.com>
2016-11-09 10:54:01 +0800

    webContextListener

--- src/main/java/spring/ioc/Main.java
+++ src/main/java/spring/ioc/Main.java
@@ -2,6 +2,7 @@ package spring.ioc;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
 
 public class Main {
 
@@ -11,6 +12,9 @@ public class Main {
 		ApplicationContext a = new ClassPathXmlApplicationContext("context.xml");
 		//Bean b =  (Bean) a.getBean("facBean");
 		Object bean = a.getBean("complicatedBean");
+		
+		WebApplicationContext context;
+		org.springframework.web.context.ContextLoaderListener listener;
 	}
 
 }
